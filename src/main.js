dafc33f3c30c6b51c0fd1d447089c9d49482797f
Your Name <you@example.com>
2024-08-12 20:49:46 -0500

    migracion a axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    Headers: {
+        'content-type': 'application/json;charset=utf-8'
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+});
+
 async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    console.log(res);
-    const data = await res.json();
+    const {data} = await api('trending/movie/day');
     const movies = data.results;
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
@@ -21,8 +29,7 @@ async function getTrendingMoviesPreview() {
 }
 
 async function getCategoryPreview () {
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-    const data = await res.json();
+    const {data} = await api('genre/movie/list');
     const categories = data.genres;
     categories.forEach(category => {
 
